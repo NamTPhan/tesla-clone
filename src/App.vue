f8a28bd02ba12658144316b8b5f479cbf51be44e
Nam <namtphann@gmail.com>
2023-01-10 15:16:42 +0100

    Add action buttons per product

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,37 @@
     desktop-bg-image="md:bg-model-y"
     mobile-bg-image="bg-mb-model-y"
     title="Model Y"
+    primary-btn-label="Custom Order"
+    primary-btn-link="https://www.tesla.com/modely/design#overview"
+    sub-btn-label="View Inventory"
+    sub-btn-link="https://www.tesla.com/inventory/new/my"
   />
   <ProductSection
     desktop-bg-image="md:bg-model-3"
     mobile-bg-image="bg-mb-model-3"
     title="Model 3"
+    primary-btn-label="Custom Order"
+    primary-btn-link="https://www.tesla.com/model3/design#overview"
+    sub-btn-label="View Inventory"
+    sub-btn-link="https://www.tesla.com/inventory/new/m3"
   />
   <ProductSection
     desktop-bg-image="md:bg-model-s"
     mobile-bg-image="bg-mb-model-s"
     title="Model S"
+    primary-btn-label="Custom Order"
+    primary-btn-link="https://www.tesla.com/models/design#overview"
+    sub-btn-label="View Inventory"
+    sub-btn-link="https://www.tesla.com/inventory/new/ms"
   />
   <ProductSection
     desktop-bg-image="md:bg-model-x"
     mobile-bg-image="bg-mb-model-x"
     title="Model X"
+    primary-btn-label="Custom Order"
+    primary-btn-link="https://www.tesla.com/modelx/design#overview"
+    sub-btn-label="View Inventory"
+    sub-btn-link="https://www.tesla.com/inventory/new/mx"
   />
   <ProductSection
     desktop-bg-image="md:bg-solar-panel"
@@ -26,6 +42,10 @@
     title="Solar Panels"
     subtitle="Lowest Cost Solar Panels in America"
     :is-subtitle-a-link="false"
+    primary-btn-label="Order Now"
+    primary-btn-link="https://www.tesla.com/energy/design"
+    sub-btn-label="Learn More"
+    sub-btn-link="https://www.tesla.com/solarpanels"
   />
   <ProductSection
     desktop-bg-image="md:bg-solar-roof"
@@ -33,12 +53,18 @@
     title="Solar Roof"
     subtitle="Produce Clean Energy From Your Roof"
     :is-subtitle-a-link="false"
+    primary-btn-label="Order Now"
+    primary-btn-link="https://www.tesla.com/solarroof/design"
+    sub-btn-label="Learn More"
+    sub-btn-link="https://www.tesla.com/solarroof"
   />
   <ProductSection
     desktop-bg-image="md:bg-charger"
     mobile-bg-image="bg-mb-charger"
     subtitle=""
     title="Accessories"
+    primary-btn-label="Shop Now"
+    primary-btn-link="https://www.tesla.com/shop?tesref=true"
   />
 </template>
 
--- src/components/ProductSection.vue
+++ src/components/ProductSection.vue
@@ -5,16 +5,39 @@
       :class="`${mobileBgImage} ${desktopBgImage}`"
       class="bg-cover bg-no-repeat bg-center h-screen w-screen justify-center flex"
     >
-      <div class="title-container">
-        <p class="text-[40px] md:text-[44px]">{{ title }}</p>
-        <a
-          v-if="subtitleLink && isSubtitleALink"
-          class="underline"
-          :href="subtitleLink"
-          target="_blank"
-          >{{ subtitle }}</a
-        >
-        <p v-if="!isSubtitleALink">{{ subtitle }}</p>
+      <div
+        class="mt-[180px] md:mt-[200px] flex flex-col justify-between w-full md:w-auto mx-8 md:mx-0"
+      >
+        <div>
+          <p class="text-[40px] md:text-[44px]">{{ title }}</p>
+          <a
+            v-if="subtitleLink && isSubtitleALink"
+            class="underline"
+            :href="subtitleLink"
+            target="_blank"
+            >{{ subtitle }}</a
+          >
+          <p v-if="!isSubtitleALink">{{ subtitle }}</p>
+        </div>
+
+        <div class="mb-28">
+          <div class="flex flex-col md:flex-row justify-center">
+            <a
+              v-if="primaryBtnLabel"
+              class="primary-action-btn"
+              :href="primaryBtnLink"
+              target="_blank"
+              >{{ primaryBtnLabel }}</a
+            >
+            <a
+              v-if="subBtnLabel"
+              class="sub-action-btn"
+              :href="subBtnLink"
+              target="_blank"
+              >{{ subBtnLabel }}</a
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -53,12 +76,34 @@ export default defineComponent({
       type: Boolean,
       default: true,
     },
+    primaryBtnLabel: {
+      type: String,
+      default: null,
+    },
+    primaryBtnLink: {
+      type: String,
+      default: null,
+    },
+    subBtnLabel: {
+      type: String,
+      default: null,
+    },
+    subBtnLink: {
+      type: String,
+      default: null,
+    },
   },
 });
 </script>
 
 <style scoped>
-.title-container {
-  @apply mt-[180px] md:mt-[200px];
+.primary-action-btn {
+  background-color: rgba(23, 26, 32, 0.8);
+  @apply py-1 px-6 text-white leading-loose min-w-[264px] min-h-[40px] rounded-[5px];
+}
+
+.sub-action-btn {
+  background-color: rgba(244, 244, 244, 0.65);
+  @apply py-1 px-6 leading-loose min-w-[264px] min-h-[40px] ml-0 md:ml-8 mt-5 md:mt-0 rounded-[5px];
 }
 </style>
