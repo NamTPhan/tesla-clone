d479d8610aa4f81dc8ec06891541350456ce2dda
Nam <namtphann@gmail.com>
2023-01-10 16:09:11 +0100

    Move product data to data method

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,18 @@
 <template>
   <NavBar />
   <ProductSection
-    desktop-bg-image="md:bg-model-y"
-    mobile-bg-image="bg-mb-model-y"
-    title="Model Y"
-    primary-btn-label="Custom Order"
-    primary-btn-link="https://www.tesla.com/modely/design#overview"
-    sub-btn-label="View Inventory"
-    sub-btn-link="https://www.tesla.com/inventory/new/my"
-  />
-  <ProductSection
-    desktop-bg-image="md:bg-model-3"
-    mobile-bg-image="bg-mb-model-3"
-    title="Model 3"
-    primary-btn-label="Custom Order"
-    primary-btn-link="https://www.tesla.com/model3/design#overview"
-    sub-btn-label="View Inventory"
-    sub-btn-link="https://www.tesla.com/inventory/new/m3"
-  />
-  <ProductSection
-    desktop-bg-image="md:bg-model-s"
-    mobile-bg-image="bg-mb-model-s"
-    title="Model S"
-    primary-btn-label="Custom Order"
-    primary-btn-link="https://www.tesla.com/models/design#overview"
-    sub-btn-label="View Inventory"
-    sub-btn-link="https://www.tesla.com/inventory/new/ms"
-  />
-  <ProductSection
-    desktop-bg-image="md:bg-model-x"
-    mobile-bg-image="bg-mb-model-x"
-    title="Model X"
-    primary-btn-label="Custom Order"
-    primary-btn-link="https://www.tesla.com/modelx/design#overview"
-    sub-btn-label="View Inventory"
-    sub-btn-link="https://www.tesla.com/inventory/new/mx"
-  />
-  <ProductSection
-    desktop-bg-image="md:bg-solar-panel"
-    mobile-bg-image="bg-mb-solar-panel"
-    title="Solar Panels"
-    subtitle="Lowest Cost Solar Panels in America"
-    :is-subtitle-a-link="false"
-    primary-btn-label="Order Now"
-    primary-btn-link="https://www.tesla.com/energy/design"
-    sub-btn-label="Learn More"
-    sub-btn-link="https://www.tesla.com/solarpanels"
-  />
-  <ProductSection
-    desktop-bg-image="md:bg-solar-roof"
-    mobile-bg-image="bg-mb-solar-roof"
-    title="Solar Roof"
-    subtitle="Produce Clean Energy From Your Roof"
-    :is-subtitle-a-link="false"
-    primary-btn-label="Order Now"
-    primary-btn-link="https://www.tesla.com/solarroof/design"
-    sub-btn-label="Learn More"
-    sub-btn-link="https://www.tesla.com/solarroof"
-  />
-  <ProductSection
-    desktop-bg-image="md:bg-charger"
-    mobile-bg-image="bg-mb-charger"
-    subtitle=""
-    title="Accessories"
-    primary-btn-label="Shop Now"
-    primary-btn-link="https://www.tesla.com/shop?tesref=true"
+    v-for="product of products"
+    :key="product.title"
+    :desktop-bg-image="product.desktopBgImage"
+    :mobile-bg-image="product.mobileBgImage"
+    :primary-btn-label="product.primaryBtnLabel"
+    :primary-btn-link="product.primaryBtnLink"
+    :sub-btn-label="product.subBtnLabel"
+    :sub-btn-link="product.subBtnLink"
+    :title="product.title"
+    :subtitle="product.subtitle"
+    :subtitle-link="product.subtitleLink"
+    :is-subtitle-a-link="product.isSubtitleALink"
   />
 </template>
 
@@ -78,6 +26,78 @@ export default defineComponent({
   components: {
     ProductSection,
     NavBar,
+  },
+  data() {
+    return {
+      products: [
+        {
+          desktopBgImage: "md:bg-model-y",
+          mobileBgImage: "bg-mb-model-y",
+          title: "Model Y",
+          primaryBtnLabel: "Custom Order",
+          primaryBtnLink: "https://www.tesla.com/modely/design#overview",
+          subBtnLabel: "View Inventory",
+          subBtnLink: "https://www.tesla.com/inventory/new/my",
+        },
+        {
+          desktopBgImage: "md:bg-model-3",
+          mobileBgImage: "bg-mb-model-3",
+          title: "Model 3",
+          primaryBtnLabel: "Custom Order",
+          primaryBtnLink: "https://www.tesla.com/model3/design#overview",
+          subBtnLabel: "View Inventory",
+          subBtnLink: "https://www.tesla.com/inventory/new/m3",
+        },
+        {
+          desktopBgImage: "md:bg-model-s",
+          mobileBgImage: "bg-mb-model-s",
+          title: "Model S",
+          primaryBtnLabel: "Custom Order",
+          primaryBtnLink: "https://www.tesla.com/models/design#overview",
+          subBtnLabel: "View Inventory",
+          subBtnLink: "https://www.tesla.com/inventory/new/ms",
+        },
+        {
+          desktopBgImage: "md:bg-model-x",
+          mobileBgImage: "bg-mb-model-x",
+          title: "Model X",
+          primaryBtnLabel: "Custom Order",
+          primaryBtnLink: "https://www.tesla.com/modelx/design#overview",
+          subBtnLabel: "View Inventory",
+          subBtnLink: "https://www.tesla.com/inventory/new/mx",
+        },
+        {
+          desktopBgImage: "md:bg-solar-panel",
+          mobileBgImage: "bg-mb-solar-panel",
+          title: "Solar Panels",
+          subtitle: "Lowest Cost Solar Panels in America",
+          isSubtitleALink: false,
+          primaryBtnLabel: "Order Now",
+          primaryBtnLink: "https://www.tesla.com/energy/design",
+          subBtnLabel: "Learn More",
+          subBtnLink: "https://www.tesla.com/solarpanels",
+        },
+        {
+          desktopBgImage: "md:bg-solar-roof",
+          mobileBgImage: "bg-mb-solar-roof",
+          title: "Solar Roof",
+          subtitle: "Produce Clean Energy From Your Roof",
+          isSubtitleALink: false,
+          primaryBtnLabel: "Order Now",
+          primaryBtnLink: "https://www.tesla.com/solarroof/design",
+          subBtnLabel: "Learn More",
+          subBtnLink: "https://www.tesla.com/solarroof",
+        },
+        {
+          desktopBgImage: "md:bg-charger",
+          mobileBgImage: "bg-mb-charger",
+          title: "Accessories",
+          subtitle: "",
+          primaryBtnLabel: "Shop Now",
+          primaryBtnLink: "https://www.tesla.com/shop?tesref=true",
+        },
+      ],
+    };
   },
 });
 </script>
